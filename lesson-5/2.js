fb751f11e4bf2cf82fb39e159f13134883f26829
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-10-08 17:58:58 +0300

    Create 2.js

---
+++ lesson-5/2.js
@@ -0,0 +1,13 @@
+function f(...args) {
+let sum = 0;
+for (let item of args) {
+  if (typeof item === 'number'){
+    sum += item;
+  } else {
+    throw new Error('Aefument is not a number');
+  }
+}
+  return sum;
+}
+console.log(f(1, 2, 3, 4, 5, 6));
+exports.f = f;
