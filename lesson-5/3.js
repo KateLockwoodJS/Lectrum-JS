556e1c6e6522f74023cec3137222ec0da653de82
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-10-10 11:41:57 +0300

    Create 3.js

---
+++ lesson-5/3.js
@@ -0,0 +1,10 @@
+function f(a, b, c) {
+  if (typeof a !== 'number' && b !== 'number' && c !== 'number'){
+   throw new Error('Argument is not a number');
+} 
+else {
+   return ((a - b) / c);
+  }
+}
+console.log(f(9, 2, 1));
+exports.f = f;
