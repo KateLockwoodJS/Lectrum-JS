26397fdd4fa0e00fe29f0538936195936f908394
Kate Lockwood <55763653+KateLockwoodJS@users.noreply.github.com>
2019-10-16 15:30:51 +0300

    Update 1.js

--- lesson-6/1.js
+++ lesson-6/1.js
@@ -1,1 +1,17 @@
+let testArray = ['a', 'b', 'c', 'd'];
 
+function testFunction(element, index, arraRef) {
+  console.log(`${index}:`, element, arraRef);
+}
+
+function forEach(arr, callback) {
+  if(!Array.isArray(arr) || typeof callback !== 'function') {
+    throw new Error('Bad arguments')
+  }
+  for (let index = 0; index < arr.length; index++) {
+    callback(arr[index], index, arr)
+  }
+}
+
+forEach(testArray, testFunction);
+exports.forEach = forEach;
